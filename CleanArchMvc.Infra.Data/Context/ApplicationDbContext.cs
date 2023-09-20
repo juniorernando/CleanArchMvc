6d9e87fd50e96cdf2bf22ec19aecae07eec34f4b
Junior Fernando <junior_618@hotmail.com>
2023-09-20 17:54:17 -0300

    atualização

--- CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,10 +1,7 @@
-﻿using Fluent.Infrastructure.FluentModel;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.EntityFrameworkCore;
+﻿using CleanArchMvc.Infra.Data.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
