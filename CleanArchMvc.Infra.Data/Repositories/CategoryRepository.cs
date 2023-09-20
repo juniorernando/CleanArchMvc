a927fe3df8a9d514d9121dcff07b1a3cb3391893
Junior Fernando <junior_618@hotmail.com>
2023-09-20 12:09:10 -0300

    Atualizaçao 1

---
+++ CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CleanArchMvc.Domain.Entities;
+
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+    }
+}
